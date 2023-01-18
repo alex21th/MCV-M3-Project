06bb8177b1333c5cdbda3e12165a184e26408063
guillem-ms <guillemmsbi@gmail.com>
2023-01-18 23:10:33 +0100

    added descriptions and hinting @ dataloader.py

--- W3/src/dataloader.py
+++ W3/src/dataloader.py
@@ -1,35 +1,41 @@
-import os
-from utils import *
-from keras.models import Sequential
-from keras.layers import Flatten, Dense, Reshape
 from keras.preprocessing.image import ImageDataGenerator
 
 
-
-
-def get_train_dataloader(directory, PATCH_SIZE, BATCH_SIZE):
+def get_train_dataloader(directory: str, patch_size: int, batch_size: int):
+    """
+    Returns a dataloader for the training set
+    :param directory: path directory of the dataset
+    :param patch_size: resize the images to patch_size x patch_size
+    :param batch_size: batch size
+    :return:
+    """
     train_datagen = ImageDataGenerator(
-          rescale=1./255,
-          horizontal_flip=True)
+        rescale=1. / 255,
+        horizontal_flip=True)
     train_generator = train_datagen.flow_from_directory(
-          directory+'/train',  # this is the target directory
-          target_size=(PATCH_SIZE, PATCH_SIZE),  # all images will be resized to PATCH_SIZExPATCH_SIZE
-          batch_size=BATCH_SIZE,
-          classes = ['coast','forest','highway','inside_city','mountain','Opencountry','street','tallbuilding'],
-          class_mode='categorical')  # since we use binary_crossentropy loss, we need categorical labels
+        directory + '/train',  # this is the target directory
+        target_size=(patch_size, patch_size),  # all images will be resized to path_size x path_size
+        batch_size=batch_size,
+        classes=['coast', 'forest', 'highway', 'inside_city', 'mountain', 'Opencountry', 'street', 'tallbuilding'],
+        class_mode='categorical')  # since we use binary_crossentropy loss, we need categorical labels
     return train_generator
-    
 
 
-
-def get_val_dataloader(directory, PATCH_SIZE, BATCH_SIZE):
+def get_val_dataloader(directory: str, patch_size: int, batch_size: int) -> ImageDataGenerator:
+    """
+    Returns a dataloader for the validation set
+    :param directory: path directory of the dataset
+    :param patch_size: resize the images to patch_size x patch_size
+    :param batch_size: batch size
+    :return: ImageDataGenerator
+    """
     train_datagen = ImageDataGenerator(
-          rescale=1./255,
-          horizontal_flip=True)
+        rescale=1. / 255,
+        horizontal_flip=True)
     train_generator = train_datagen.flow_from_directory(
-          directory+'/test',  # this is the target directory
-          target_size=(PATCH_SIZE, PATCH_SIZE),  # all images will be resized to PATCH_SIZExPATCH_SIZE
-          batch_size=BATCH_SIZE,
-          classes = ['coast','forest','highway','inside_city','mountain','Opencountry','street','tallbuilding'],
-          class_mode='categorical')  # since we use binary_crossentropy loss, we need categorical labels
+        directory + '/test',  # this is the target directory
+        target_size=(patch_size, patch_size),  # all images will be resized to PATCH_SIZExPATCH_SIZE
+        batch_size=batch_size,
+        classes=['coast', 'forest', 'highway', 'inside_city', 'mountain', 'Opencountry', 'street', 'tallbuilding'],
+        class_mode='categorical')  # since we use binary_crossentropy loss, we need categorical labels
     return train_generator
