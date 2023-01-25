a708c7b44dfb54df4f8072f44a0089cbd1907e0d
guillem-ms <guillemmsbi@gmail.com>
2023-01-25 20:14:52 +0100

    task 1 wandb logging

--- W4/fine_tuning.py
+++ W4/fine_tuning.py
@@ -1,17 +1,12 @@
 import os
 from argparse import ArgumentParser
 
-from keras.layers import Dropout
-from tensorflow.keras.layers import Flatten
-
 import wandb
 from wandb.keras import WandbMetricsLogger
-from tensorflow.keras.applications.mobilenet import MobileNet
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.models import Model
-from tensorflow.keras.utils import plot_model
+
 
 from W4.src.dataloader import get_train_dataloader, get_val_dataloader
+from W4.src.models import get_model
 from W4.src.optimizers import get_lr, get_optimizer
 from W4.src.utils import prepare_gpu, plot_metrics_and_losses, load_config_from_yaml
 
@@ -28,39 +23,36 @@ def parse_args():
 
 def main(params):
     config = load_config_from_yaml(params.config)
-    lr_schedule_config = config["lr_scheduler"]
-    out_dir = config["output_path"]
-    optimizer_config = config["optimizer"]
+
+    # unpack config dictionaries
     wandb_config = config["wandb"]
+    lr_schedule_config = config["lr_scheduler"]
+    optimizer_config = config["optimizer"]
     data_config = config["dataloaders"]
+    early_stop_config = config["early_stopping"]
+    model_config = config['model']
+
+    # unpack useful values from config dictionaries
     input_size = data_config["input_size"]
     batch_size = data_config["batch_size"]
     epochs = lr_schedule_config["params"]["num_epochs"]
-    base_lr = lr_schedule_config["params"]["base_lr"]
     data_dir = data_config["data_path"]
     inference_batch_size = data_config["inference_batch_size"]
-
-    wandb.config = {
-        "learning_rate": base_lr,
-        "lr_scheduler": lr_schedule_config["type"],
-        "epochs": epochs,
-        "batch_size": batch_size
-    }
-
-    wandb.init(project=wandb_config['project'], entity=wandb_config['entity'], config=wandb.config)
-
+    out_dir = config["output_path"]
     os.makedirs(out_dir, exist_ok=True)
 
-    base_model = MobileNet(weights='imagenet', include_top=False, input_shape=(input_size, input_size, 3))
-    base_model.trainable = False
-    base_model.summary()
-    plot_model(base_model, to_file=out_dir + 'base_mobilenet.png', show_shapes=True, show_layer_names=True)
+    run_config = {
+        'model': model_config['name'],
+        "lr_scheduler": lr_schedule_config,
+        "optimizer": optimizer_config,
+        "batch_size": batch_size,
+        'epochs': epochs,
+        "early_stopping": early_stop_config
+    }
 
-    head_model = Flatten(name="flatten")(base_model.output)
-    head_model = Dense(8, activation='softmax', name='predictions')(head_model)
+    wandb.init(project=wandb_config['project'], entity=wandb_config['entity'], config=run_config)
 
-    model = Model(inputs=base_model.input, outputs=head_model)
-    plot_model(model, to_file=out_dir + 'finetuning_mobilenet.png', show_shapes=True, show_layer_names=True)
+    model = get_model(model_name=model_config['name'], out_dir=out_dir)
 
     train_dataloader = get_train_dataloader(directory=data_dir, patch_size=input_size,
                                             batch_size=batch_size)
